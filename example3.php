94fc0d80d8ed503617bdf6d6d5bd50441f1cac3d
Adam Willard <awillard@gmail.com>
2023-01-11 09:53:03 -0500

    Update example3.php

--- example3.php
+++ example3.php
@@ -1,3 +1,3 @@
-<h1>Think about what url format creates a XSS payload and how to add event handlers to execute XSS</h1>
+<h1>Think about what url format creates a XSS payload, how to add event handlers to execute XSS and how to add a HTML element.</h1>
 
 <a href="<?php echo $_GET["url"];?>">There are a few payloads that can inject XSS here.</a>
