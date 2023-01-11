c7daf1bf7bd14908cd1fb2252c68287f09a622ad
Adam Willard <awillard@gmail.com>
2023-01-11 17:03:39 -0500

    standardize the parameters

--- example3.php
+++ example3.php
@@ -1,3 +1,3 @@
 <h1>Think about what url format creates a XSS payload, how to add event handlers to execute XSS and how to add a HTML element.</h1>
 
-<a href="<?php echo $_GET["url"];?>">There are a few payloads that can inject XSS here.</a>
+<a href="<?php echo $_GET["xss"];?>">There are a few payloads that can inject XSS here.</a>
