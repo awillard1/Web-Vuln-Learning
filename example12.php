fcbb2eee8d9ba8b24770c74a1308b1c7cb29bfe6
Adam Willard <awillard@gmail.com>
2023-01-11 16:37:03 -0500

    Add files via upload

---
+++ example12.php
@@ -0,0 +1,17 @@
+<script>
+function logme(val){
+	console.log(val);
+}
+</script>
+<body onload="logme('<?php
+$string = str_ireplace('"','',str_ireplace('>','',str_ireplace('<','',str_ireplace('script','',$_GET["script"]))));
+$string = str_ireplace('*','',$string);
+$string = str_ireplace('-','',$string);
+$string = str_ireplace('+','',$string);
+$string = str_ireplace('/','',$string);
+$string = str_ireplace('%','',$string);
+$string = str_ireplace(':','',$string);
+$string = str_ireplace('{','',$string);
+$string = str_ireplace('}','',$string);
+echo $string;
+?>');"><p>The goal is to alert document.domain when the page loads.</body>
