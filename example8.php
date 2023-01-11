d600e0db07aa5d65d7afd064e23d0ea2d154cf77
Adam Willard <awillard@gmail.com>
2023-01-11 12:42:43 -0500

    Add files via upload

---
+++ example8.php
@@ -0,0 +1,4 @@
+<p>alert any string</p>
+<script>
+var x = 'helloworld<?php echo str_replace('alert','',str_replace('+','',str_replace('document','',str_replace('(','',str_replace(';','',str_replace('/','',str_ireplace('</script>','',$_GET["v"])))))));?>';
+</script>
