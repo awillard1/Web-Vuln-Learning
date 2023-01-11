aba7d2c475db86b64cbe6e84259c54d8990facba
Adam Willard <awillard@gmail.com>
2023-01-11 10:41:55 -0500

    Add files via upload

---
+++ example5.php
@@ -0,0 +1,5 @@
+<p>Alert the value of document.domain</p>
+<p><a href='https://gist.github.com/awillard1'>Look for the js_xss_payloads.js gist file.</a><br>This file changes often so a direct link would change each update.</p>
+<script>
+var x = 'helloworld<?php echo str_replace('document.','',str_replace(';','',str_replace('/','',str_ireplace('</script>','',$_GET["val"]))));?>';
+</script>
