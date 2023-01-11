d251f90313d3d595bba88ea5923a18d7768228f8
Adam Willard <awillard@gmail.com>
2023-01-11 17:07:12 -0500

    standardize the parameters

--- example11.php
+++ example11.php
@@ -3,7 +3,7 @@
 <p>HINT: <a href='https://gist.github.com/awillard1'>Look for the js_xss_payloads.js gist file.</a><br>The js_xss_payloads.js file changes often so a direct link on gist would change with each update.</p>
 <script>
 <?php
-$string = $_GET["script"];
+$string = $_GET["xss"];
 if (str_contains($string,'<'))
 	return;
 if (str_contains($string,'\''))
