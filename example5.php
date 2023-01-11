9a9c9ed9e9708408c9e46a6533ce64c2013c9ee1
Adam Willard <awillard@gmail.com>
2023-01-11 17:04:28 -0500

    standardize the parameters

--- example5.php
+++ example5.php
@@ -1,5 +1,5 @@
 <p>Alert the value of document.domain</p>
 <p>HINT: <a href='https://gist.github.com/awillard1'>Look for the js_xss_payloads.js gist file.</a><br>The js_xss_payloads.js file changes often so a direct link on gist would change with each update.</p>
 <script>
-var x = 'helloworld<?php echo str_replace('document.','',str_replace(';','',str_replace('/','',str_ireplace('</script>','',$_GET["val"]))));?>';
+var x = 'helloworld<?php echo str_replace('document.','',str_replace(';','',str_replace('/','',str_ireplace('</script>','',$_GET["xss"]))));?>';
 </script>
