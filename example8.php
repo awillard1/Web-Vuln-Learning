5beaf1e1b1ccfcfca64784b14d83dfaf2b761109
Adam Willard <awillard@gmail.com>
2023-01-12 07:34:06 -0500

    Update example8.php

--- example8.php
+++ example8.php
@@ -1,4 +1,4 @@
 <p>alert any string</p>
 <script>
-var x = 'helloworld<?php echo str_replace('alert','',str_replace('+','',str_replace('document','',str_replace('(','',str_replace(';','',str_replace('/','',str_ireplace('</script>','',$_GET["xss"])))))));?>';
+var x = 'helloworld<?php echo str_ireplace('script','',str_replace('alert','',str_replace('+','',str_replace('document','',str_replace('(','',str_replace(';','',str_replace('/','',str_ireplace('</script>','',$_GET["xss"]))))))));?>';
 </script>
