e9559f33765f070efeebb30e95d77ca2c0db2b91
Adam Willard <awillard@gmail.com>
2023-01-12 14:32:17 -0500

    Update example13.php

--- example13.php
+++ example13.php
@@ -1,5 +1,5 @@
 <h1 id=test>What a about those parameters? A <r>ray of possibilities!</h1>
-Get &lt;a href="javascript:alert(document.domain)"&gtclick me</a> to be written to the page.
+Get &lt;a href="javascript:alert(document.domain)"&gtclick me&lt;/a&gt; to be written to the page.
 <p><?php 
 $i=0;
 $params = $_GET["xss"];
