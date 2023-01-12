766877769fbe8f7e6cf777bdcd2cf34949f298fd
Adam Willard <awillard@gmail.com>
2023-01-12 07:27:44 -0500

    Update example4.php

--- example4.php
+++ example4.php
@@ -2,7 +2,7 @@
 <script>
 function xyz(){
 	if (1===1){ 
-		var zyx ='hello<?php echo str_ireplace('</script>','',$_GET["xss"]);?>';
+		var zyx ='hello<?php echo str_ireplace('script','',str_ireplace('</script>','',$_GET["xss"]));?>';
 		if (2==2){
 			document.getElementById('test').innerText='hello';
 		}
