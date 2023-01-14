5798e2ad02bdc7234f95aa8f07acb2d1967d1665
Adam Willard <awillard@gmail.com>
2023-01-14 12:35:55 -0500

    xxe

---
+++ xxe/example01.php
@@ -0,0 +1,21 @@
+<html><head>
+    <style>
+        body {font-family:arial}
+        li {padding:2px}
+        h1 {border-bottom: 5px solid black;margin: 15px 0px 5px 0px;
+    display: inline-block;}
+        </style>
+</head>
+<body>
+<h1>Edit the file in the xxe/files/xxe.xml</h1>
+<p><?php 
+    libxml_disable_entity_loader (false);
+    $xmlfile = file_get_contents('files/xxe.xml');
+    $dom = new DOMDocument();
+    $dom->loadXML($xmlfile, LIBXML_NOENT | LIBXML_DTDLOAD);
+    $data = simplexml_import_dom($dom);
+    $xml = $data->saveXML();
+    echo htmlentities($xml);
+?></p>
+</body>
+</html>
